04e9ea1aa44d54c0e4b6454cb2f89438ead5acc0
AP <a@a.com>
2014-01-10 13:57:43 -0800

    Creating setup in MovieServicetest.java

--- src/test/java/moviebase/MovieServiceTest.java
+++ src/test/java/moviebase/MovieServiceTest.java
@@ -4,18 +4,38 @@ import static org.junit.Assert.assertEquals;
 import moviebase.Movie;
 import moviebase.MovieService;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
-
 public class MovieServiceTest {
-	
+
+	private MovieService movieService;
+
+	@BeforeMethod
+	public void setUp() {
+		movieService = new MovieService("VMware Movie Night");
+	}
+
 	@Test
 	public void addAMovieandGetTheTotal() {
-		//subject under test
-		MovieService movieService = new MovieService("VMware Movie Night");
+		// subject under test
 		movieService.addMovie(new Movie("E.T- The Extra Terrestial"));
 		movieService.addMovie(new Movie("Wall-E"));
 		assertEquals(movieService.getCount(), 2);
+	}
+
+	@Test
+	public void addOneMovieAndGetTheTotal() {
+		// subject under test
+		movieService.addMovie(new Movie("Wall-E"));
+		assertEquals(movieService.getCount(), 1);
+
+	}
+
+	@Test
+	public void getTheTotalWithNoMovies() {
+		// subject under test
+		assertEquals(movieService.getCount(), 0);
+
 	}
 }
